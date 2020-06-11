5c81d29bb41a5c9979ffbc89b99a442c5353f5a7
Ivan <vaneck2003@gmail.com>
2020-06-11 14:15:39 +0300

    Update settings Add game activity

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +15,16 @@ class SettingsActivity : AppCompatActivity(){
         exit.setOnClickListener {
             finish()
         }
+
+        gameButton.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            intent.putExtra("difficulty", spinner.selectedItem.toString())
+            intent.putExtra("mode", spinner2.selectedItem.toString())
+            startActivity(intent)
+        }
+        //Подчёркивание
+        //textView4.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        //textView5.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
         val difficulty = arrayOf("Легкая", "Средняя", "Сложная")
         val adapter = ArrayAdapter(this,R.layout.spinner_item,difficulty)
