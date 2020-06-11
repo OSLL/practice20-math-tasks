ccb3dafcf4a91ef6121b2b107e0e195e70667a28
Ivan <vaneck2003@gmail.com>
2020-06-11 11:07:03 +0300

    Remake settings

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 
@@ -12,5 +13,13 @@ class SettingsActivity : AppCompatActivity(){
         exit.setOnClickListener {
             finish()
         }
+
+        val difficulty = arrayOf("Легкая", "Средняя", "Сложная")
+        val adapter = ArrayAdapter(this,R.layout.spinner_item,difficulty)
+        spinner.adapter = adapter
+
+        val mode = arrayOf("Расставить знаки", "Расставить цифры", "Комбинированный")
+        val adapter2 = ArrayAdapter(this,R.layout.spinner_item,mode)
+        spinner2.adapter = adapter2
     }
 }
