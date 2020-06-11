031c55fc6659f7e5411d907d3616ef59ae1b6525
Reguas <SlonIdetnaGithub>
2020-06-11 12:15:55 +0600

    Prettify code Fixed minor bugs

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/Solver.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Solver.kt
@@ -5,6 +5,7 @@ class Solver {
     private var pos = -1
     private var currNumber = 0
     private var currToken = 0.toChar()
+    private val ops = "+-*/@%|&^"
 
     private fun getNextToken() {
         pos++
@@ -12,22 +13,18 @@ class Solver {
             currToken = 'q'
         } else {
             when (expression[pos]) {
-                '+', '-', '*', '/', '@', '%', '(', ')', '|', '&', '^' -> {
+                in ops, '(', ')' -> {
                     currToken = expression[pos]
                 }
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                in '0'..'9' -> {
                     currToken = 'n'
                     val pos0 = pos
                     while (pos < expression.length && expression[pos].isDigit()) pos++
-                    try {
-                        currNumber = expression.substring(pos0, pos).toInt()
-                    } catch (e: NumberFormatException) {
-                        currToken = 'i'
-                    }
+                    currNumber = expression.substring(pos0, pos).toInt()
+
                     pos--
                 }
                 else -> {
-                    //out.println("Incorrect symbol at position " + (pos+1));
                     currToken = 'i'
                 }
             }
@@ -46,81 +43,17 @@ class Solver {
             }
             '(' -> {
                 getNextToken()
-                val e = expr()
+                val e = operPlus()
                 getNextToken()
                 e
             }
             else -> {
-                expr()
+                operOr()
             }
         }
     }
 
-    private fun expr(): Int {
-        var left = term()
-        while (true) {
-            when (currToken) {
-                '+' -> {
-                    getNextToken()
-                    left += term()
-                }
-                '-' -> {
-                    getNextToken()
-                    left -= term()
-                }
-                else -> {
-                    return left
-                }
-            }
-        }
-    }
-
-    private fun or() : Int {
-        var left = xor()
-        while (true) {
-            when (currToken) {
-                '|' -> {
-                    getNextToken()
-                    left = left or term()
-                }
-                else -> {
-                    return left
-                }
-            }
-        }
-    }
-
-    private fun and() : Int {
-        var left = expr()
-        while (true) {
-            when (currToken) {
-                '&' -> {
-                    getNextToken()
-                    left = left and term()
-                }
-                else -> {
-                    return left
-                }
-            }
-        }
-    }
-
-    private fun xor() : Int {
-        var left = and()
-        while (true) {
-            when (currToken) {
-                '^' -> {
-                    getNextToken()
-                    left = left xor term()
-                }
-                else -> {
-                    return left
-                }
-            }
-        }
-    }
-
-    private fun term(): Int {
+    private fun operMult(): Int {
         var left = prim()
         while (true) {
             when (currToken) {
@@ -147,39 +80,101 @@ class Solver {
         }
     }
 
+    private fun operPlus(): Int {
+        var left = operMult()
+        while (true) {
+            when (currToken) {
+                '+' -> {
+                    getNextToken()
+                    left += operMult()
+                }
+                '-' -> {
+                    getNextToken()
+                    left -= operMult()
+                }
+                else -> {
+                    return left
+                }
+            }
+        }
+    }
+
+    private fun operAnd() : Int {
+        var left = operPlus()
+        while (true) {
+            when (currToken) {
+                '&' -> {
+                    getNextToken()
+                    left = left and operMult()
+                }
+                else -> {
+                    return left
+                }
+            }
+        }
+    }
+
+    private fun operXor() : Int {
+        var left = operAnd()
+        while (true) {
+            when (currToken) {
+                '^' -> {
+                    getNextToken()
+                    left = left xor operMult()
+                }
+                else -> {
+                    return left
+                }
+            }
+        }
+    }
+
+    private fun operOr() : Int {
+        var left = operXor()
+        while (true) {
+            when (currToken) {
+                '|' -> {
+                    getNextToken()
+                    left = left or operMult()
+                }
+                else -> {
+                    return left
+                }
+            }
+        }
+    }
+
     private fun checkExpression(): Boolean {
-        expression = expression.replace(',', '.')
         expression = expression.replace(" ", "")
         pos = -1
         var res = true
-        if (expression.length == 0) res = false
-        var prev_token = '('
+        if (expression.isEmpty()) res = false
+        var prevToken = '('
         getNextToken()
         while (currToken != 'q') {
             if (currToken == 'i') res = false
-            when (prev_token) {
-                'n' -> {
-                    if (currToken == '(') {
-                        expression = expression.substring(
-                            0,
-                            pos
-                        ) + '*' + expression.substring(pos)
+            when (prevToken) {
+                'n', ')' -> {
+                    if (currToken == '(' || currToken == 'n') {
+                        expression = expression.substring(0, pos) + '*' + expression.substring(pos)
                         pos++
                     }
                 }
-                '+', '-', '*', '/', '^', '(' -> {
-                    if (currToken == '+' || currToken == '*' || currToken == '/' || currToken == '^') res =
-                        false
+                in ops, '(' -> {
+                    when(currToken) {
+                        in ops, ')' -> res = false
+                    }
                 }
             }
-            prev_token = currToken
+            prevToken = currToken
             getNextToken()
         }
-        if (prev_token == '+' || prev_token == '*' || prev_token == '/' || prev_token == '^' || prev_token == '-' || prev_token == '(') res =
-            false
+        when(prevToken) {
+            in ops, '(' -> res = false
+        }
         var braces = 0
-        for (i in 0 until expression.length) {
-            if (expression[i] == '(') braces++ else if (expression[i] == ')') braces--
+        for (e in expression) {
+            if (e == '(') braces++ else if (e == ')') braces--
         }
         if (braces != 0) res = false
         pos = -1
@@ -190,8 +185,9 @@ class Solver {
         expression = expr
         if (checkExpression()) {
             getNextToken()
-            return or()
+            return operOr()
         }
         return 0
     }
 }
+
