01196cf59e573ffdf4aaf68338ac922f6367a632
Reguas <SlonIdetnaGithub>
2020-06-16 21:37:31 +0600

    Crutch file fix

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/game2gen.kt
@@ -0,0 +1,42 @@
+package com.makentoshe.androidgithubcitemplate;
+fun gen2_game(hard:Int):String{
+    var operations:String = " +-*^%&|"
+    var ans:String=""
+    if(hard==1){
+        val num=(1..4).random()
+        for(i in 0..num) {
+            ans += (1..9).random()
+            if (i != num) {
+                var opo:Char= operations[(0..3).random()]
+                if(opo!=' ')
+                    ans+=opo
+            }
+        }
+        return ans
+    }
+    if(hard==2){
+        val num=(4..7).random()
+        for(i in 0..num){
+            ans+=(1..9).random()
+            if(i!=num) {
+                var opo:Char= operations[(0..3).random()]
+                if(opo!=' ')
+                    ans+=opo
+            }
+        }
+        return ans
+    }
+    if(hard==3){
+        val num=(7..10).random()
+        for(i in 0..num) {
+            ans += (1..9).random()
+            if (i != num) {
+                var opo: Char = operations[(0..7).random()]
+                if (opo != ' ')
+                    ans += opo
+            }
+        }
+        return ans
+    }
+    return ""
+}
