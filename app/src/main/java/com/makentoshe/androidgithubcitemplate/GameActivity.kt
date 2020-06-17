2c2ac810d1631e9bee591c54e627c0543fdfeba3
Plass <sir.assonov@yandex.ru>
2020-06-17 12:11:26 +0300

    фиговый ввод получился, Ване на исправенье

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
@@ -1,16 +1,13 @@
 package com.makentoshe.androidgithubcitemplate
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.ContextMenu
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game.*
+
 
 class GameActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +34,53 @@ class GameActivity: AppCompatActivity() {
         }
         button.setOnClickListener {
             menu.show()
+        }
+        var predznazh = "=-+*"
+        var Change = true
+        if (intent.getStringExtra("mode") == "Расставить цифры") {
+            primer.setText("=-+*")
+            primer.addTextChangedListener(object : TextWatcher {
+                override fun onTextChanged(
+                    s: CharSequence,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                }
+                override fun afterTextChanged(arg: Editable) {
+                    if (Change) {
+                        var counter1 = 0
+                        var counter2 = 0
+                        for (j in arg.toString()) if (!j.isDigit() || j in arrayOf('<', '>', '=', '+', '-', '*', '/', '^')) Change = false
+                        for (i in 0..(arg.toString().length - 1)) {
+                            if (arg.toString()[i] in arrayOf('<', '>', '=', '+', '-', '*', '/', '^')) {
+                                counter1 += 1
+                            }
+                        }
+                        for (i in predznazh) {
+                            if (i in arrayOf('<', '>', '=', '+', '-', '*', '/', '^')) {
+                                counter2 += 1
+                            }
+                        }
+                        if (counter1 != counter2) {
+                            Change = false
+                        }
+                    }
+                    else{
+                        Change = true
+                    }
+                    if (!Change) {
+                        primer.setText(predznazh)
+                    }
+                    predznazh = primer.text.toString()
+                }
+                override fun beforeTextChanged(
+                    s: CharSequence,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {}
+            })
         }
     }
 }
