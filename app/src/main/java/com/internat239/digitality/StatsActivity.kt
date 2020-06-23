a2fe857510cd4bde782879bad54739320ed396f4
Ivan <vaneck2003@gmail.com>
2020-06-23 21:53:41 +0300

    add activity fixed design

--- app/src/main/java/com/internat239/digitality/StatsActivity.kt
+++ app/src/main/java/com/internat239/digitality/StatsActivity.kt
@@ -6,10 +6,26 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_stats.*
 
 class StatsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
 
+        var stat:Statistics
+        var statsString = "You not play yet"
+
+        try {
+            stat = Statistics.get(getString(R.string.stat_path),this)
+            statsString = "Count of games: ${stat.gamesCount} \n" +
+                    "Average time: ${stat.timeAverage()} \n" +
+                    "Average moves: ${stat.movesAverage()} \n" +
+                    "Average hints: ${stat.hintsCountAverage()}"
+        }catch (e:Exception){}
+
+
+        statsView.text = statsString
+
+        exit.setOnClickListener {
+            finish()
+        }
     }
 }
